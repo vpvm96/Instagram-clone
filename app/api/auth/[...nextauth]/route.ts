7cd6a5064ef7079b9020907fa78ad0252af86c2d
vpvm96 <vpvm96@gmail.com>
2023-05-03 15:09:12 +0900

    feat: 로그인시 sanity에 로그인 정보 저장 및 session 값 받기

--- app/api/auth/[...nextauth]/route.ts
+++ app/api/auth/[...nextauth]/route.ts
@@ -1,5 +1,6 @@
 import NextAuth from "next-auth"
 import GoogleProvider from "next-auth/providers/google"
+import { addUser } from "@/service/user"
 
 const handler = NextAuth({
   providers: [
@@ -8,6 +9,33 @@ const handler = NextAuth({
       clientSecret: process.env.GOOGLE_OAUTH_SECRET || "",
     }),
   ],
+  callbacks: {
+    async signIn({ user: { id, email, name, image } }) {
+      if (!email) return false
+      addUser({
+        id,
+        email,
+        name: name || "",
+        image,
+        username: email.split("@")[0],
+      })
+      return true
+    },
+    async session({ session }) {
+      console.log("session", session)
+      const user = session.user
+      if (user) {
+        session.user = {
+          ...user,
+          username: user.email?.split("@")[0] || "",
+        }
+      }
+      return session
+    },
+  },
+  pages: {
+    signIn: "/auth/signin",
+  },
 })
 
 export { handler as GET, handler as POST }
