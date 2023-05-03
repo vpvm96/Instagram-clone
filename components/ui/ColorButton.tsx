34aa82a5b668c0bf044e7243374384121dfd0b25
vpvm96 <vpvm96@gmail.com>
2023-05-03 15:06:55 +0900

    fix: ColorButton size props 추가 적용

--- components/ui/ColorButton.tsx
+++ components/ui/ColorButton.tsx
@@ -1,13 +1,20 @@
 interface Props {
   text: string
+  size?: "small" | "large"
   onClick: () => void
 }
 
-export default function ColorButton({ text, onClick }: Props) {
+export default function ColorButton({ text, size = "small", onClick }: Props) {
   return (
-    <div className="rounded-md bg-gradient-to-bl from-fuchsia-600 via-rose-500 to-amber-300 p-[0.15rem]">
+    <div
+      className={`rounded-md bg-gradient-to-bl from-fuchsia-600 via-rose-500 to-amber-300 ${
+        size === "large" ? "p-[0.3rem]" : "p-[0.15rem]"
+      }`}
+    >
       <button
-        className="bg-white rounded-sm text-base p-[0.3rem] hover:opacity-90 transition-opacity"
+        className={`bg-white rounded-sm text-base hover:opacity-90 transition-opacity ${
+          size === "large" ? "p-4 text-2xl" : "p-[0.3rem] text-base"
+        }`}
         onClick={onClick}
       >
         {text}
