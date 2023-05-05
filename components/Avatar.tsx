eb7b6629b780e627dcc1d06c2b84b6304a3550ab
vpvm96 <vpvm96@gmail.com>
2023-05-05 20:03:13 +0900

    feat: Avatar Custom 스타일 props 지정 및 적용

--- components/Avatar.tsx
+++ components/Avatar.tsx
@@ -2,19 +2,33 @@ import Image from "next/image"
 
 interface Props {
   image?: string | null
+  size: "small" | "normal"
+  highlight?: boolean
 }
 
-export default function Avatar({ image }: Props) {
+export default function Avatar({
+  image,
+  size = "normal",
+  highlight = false,
+}: Props) {
   return (
-    <div className="rounded-full bg-gradient-to-bl from-fuchsia-600 via-rose-500 to-amber-300">
+    <div className={getContainerStyle(highlight)}>
       <Image
         src={image ?? ""}
         alt="avatar"
-        width={40}
-        height={40}
+        width={size === "small" ? 40 : 60}
+        height={size === "small" ? 40 : 60}
         referrerPolicy="no-referrer"
-        className="rounded-full p-[0.1rem]"
+        className="bg-white rounded-full p-[0.15rem]"
       />
     </div>
   )
+}
+
+function getContainerStyle(highlight: boolean): string {
+  const baseStyle = "rounded-full flex justify-center items-center"
+  const highlightStyle = highlight
+    ? "bg-gradient-to-bl from-fuchsia-600 via-rose-500 to-amber-300 p-[0.05rem]"
+    : ""
+  return `${baseStyle} ${highlightStyle}`
 }
