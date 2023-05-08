02f3d7d49a90467de5da32465a24b62e789d3ad2
vpvm96 <vpvm96@gmail.com>
2023-05-09 00:55:41 +0900

    feat: FollowingBar 스타일링

--- components/FollowingBar.tsx
+++ components/FollowingBar.tsx
@@ -11,19 +11,24 @@ export default function FollowingBar() {
   const users = data?.following
 
   return (
-    <section>
+    <section className="w-full flex justify-center items-center p-4 shadow-sm shadow-neutral-300 mb-4 rounded-lg min-h-[90px] overflow-x-auto">
       {isLoading ? (
         <PropagateLoader size={8} color="red" />
       ) : (
         (!users || users.length === 0) && <p>{`You don't have following`}</p>
       )}
       {users && users.length > 0 && (
-        <ul>
+        <ul className="w-full flex gap-2">
           {users.map(({ image, username }) => (
             <li key={username}>
-              <Link href={`/user/${username}`}>
+              <Link
+                className="flex flex-col items-center w-20"
+                href={`/user/${username}`}
+              >
                 <Avatar image={image} size="normal" highlight />
-                <p>{username}</p>
+                <p className="w-full text-center text-sm text-ellipsis overflow-hidden">
+                  {username}
+                </p>
               </Link>
             </li>
           ))}
