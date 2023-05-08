8d012d354ce2ea95968a3eb066d70415a4bca47b
vpvm96 <vpvm96@gmail.com>
2023-05-08 23:48:44 +0900

    feat: FollowingBar 컴포넌트 생성 및 팔로잉 목록 보여주기 적용

--- components/FollowingBar.tsx
+++ components/FollowingBar.tsx
@@ -1,3 +1,34 @@
+"use client"
+
+import useSWR from "swr"
+import { DetailUser } from "@/model/user"
+import { PropagateLoader } from "react-spinners"
+import Link from "next/link"
+import Avatar from "./Avatar"
+
 export default function FollowingBar() {
-  return <div>Following Bar</div>
+  const { data, isLoading, error } = useSWR<DetailUser>("/api/me")
+  const users = data?.following
+
+  return (
+    <section>
+      {isLoading ? (
+        <PropagateLoader size={8} color="red" />
+      ) : (
+        (!users || users.length === 0) && <p>{`You don't have following`}</p>
+      )}
+      {users && users.length > 0 && (
+        <ul>
+          {users.map(({ image, username }) => (
+            <li key={username}>
+              <Link href={`/user/${username}`}>
+                <Avatar image={image} size="normal" highlight />
+                <p>{username}</p>
+              </Link>
+            </li>
+          ))}
+        </ul>
+      )}
+    </section>
+  )
 }
