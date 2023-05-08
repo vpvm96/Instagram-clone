3383185fd08bd7a9a6caaefa206c55fa6f292af3
vpvm96 <vpvm96@gmail.com>
2023-05-08 23:48:12 +0900

    feat: Sanity의 나의 정보 데이터를 받아오는 api 추가

---
+++ app/api/me/route.ts
@@ -0,0 +1,15 @@
+import { NextResponse } from "next/server"
+import { getServerSession } from "next-auth"
+import { authOptions } from "@/pages/api/auth/[...nextauth]"
+import { getUserByUsername } from "@/service/user"
+
+export async function GET(requset: Request) {
+  const session = await getServerSession(authOptions)
+  const user = session?.user
+
+  if (!user) return new Response("Authentication Error", { status: 401 })
+
+  return getUserByUsername(user.username).then((data) =>
+    NextResponse.json(data)
+  )
+}
