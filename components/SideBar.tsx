6b5196b016e96d42abe26f9bc5d56fd1d7d718d3
vpvm96 <vpvm96@gmail.com>
2023-05-09 22:10:34 +0900

    fix: Avatar 사이즈 재조정으로 인한 에러 수정

--- components/SideBar.tsx
+++ components/SideBar.tsx
@@ -9,7 +9,7 @@ export default function SideBar({ user: { name, username, image } }: Props) {
   return (
     <>
       <div className="flex items-center">
-        {image && <Avatar image={image} size="normal" />}
+        {image && <Avatar image={image} />}
         <div className="ml-4">
           <p className="font-bold">{username}</p>
           <p className="text-lg text-neutral-500 leading-4">{name}</p>
