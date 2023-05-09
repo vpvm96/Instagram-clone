4d3e8038ee0f0ad15d9479153c0c8881d1a4c9bb
vpvm96 <vpvm96@gmail.com>
2023-05-09 22:13:20 +0900

    feat: carousel 라이브러리 적용으로 인한 로직 수정

--- components/FollowingBar.tsx
+++ components/FollowingBar.tsx
@@ -5,11 +5,11 @@ import { DetailUser } from "@/model/user"
 import { PropagateLoader } from "react-spinners"
 import Link from "next/link"
 import Avatar from "./Avatar"
+import ScrollableBar from "./ui/ScrollableBar"
 
 export default function FollowingBar() {
   const { data, isLoading, error } = useSWR<DetailUser>("/api/me")
   const users = data?.following
-
   return (
     <section className="w-full flex justify-center items-center p-4 shadow-sm shadow-neutral-300 mb-4 rounded-lg min-h-[90px] overflow-x-auto">
       {isLoading ? (
@@ -18,21 +18,20 @@ export default function FollowingBar() {
         (!users || users.length === 0) && <p>{`You don't have following`}</p>
       )}
       {users && users.length > 0 && (
-        <ul className="w-full flex gap-2">
+        <ScrollableBar>
           {users.map(({ image, username }) => (
-            <li key={username}>
-              <Link
-                className="flex flex-col items-center w-20"
-                href={`/user/${username}`}
-              >
-                <Avatar image={image} size="normal" highlight />
-                <p className="w-full text-center text-sm text-ellipsis overflow-hidden">
-                  {username}
-                </p>
-              </Link>
-            </li>
+            <Link
+              key={username}
+              className="flex flex-col items-center w-20"
+              href={`/user/${username}`}
+            >
+              <Avatar image={image} highlight />
+              <p className="w-full text-center text-sm text-ellipsis overflow-hidden">
+                {username}
+              </p>
+            </Link>
           ))}
-        </ul>
+        </ScrollableBar>
       )}
     </section>
   )
