f4a59f0bed85ecc56e9f7367f610dfe6a29b59a7
vpvm96 <vpvm96@gmail.com>
2023-05-09 22:12:46 +0900

    feat: 최상위 PostList 컴포넌트 완성

--- components/PostList.tsx
+++ components/PostList.tsx
@@ -1,3 +1,29 @@
+"use client"
+
+import useSWR from "swr"
+import { SimplePost } from "@/model/post"
+import GridSpinner from "./ui/GridSpinner"
+import PostListCard from "./PostListCard"
+
 export default function PostList() {
-  return <div>PostList</div>
+  const { data, isLoading } = useSWR<SimplePost[]>("api/posts")
+
+  return (
+    <section>
+      {isLoading && (
+        <div className="text-center mt-32">
+          <GridSpinner />
+        </div>
+      )}
+      {data && (
+        <ul>
+          {data.map((post, index) => (
+            <li key={post.id} className="mb-4">
+              <PostListCard post={post} priority={index < 2} />
+            </li>
+          ))}
+        </ul>
+      )}
+    </section>
+  )
 }
