5dc467b2d50491f1ae6d28c5dd6813cfaab07c34
vpvm96 <vpvm96@gmail.com>
2023-05-10 18:50:21 +0900

    fix: carousel z-index로 인한 layout 문제로 0으로 변경

--- components/FollowingBar.tsx
+++ components/FollowingBar.tsx
@@ -11,7 +11,7 @@ export default function FollowingBar() {
   const { data, isLoading, error } = useSWR<DetailUser>("/api/me")
   const users = data?.following
   return (
-    <section className="w-full flex justify-center items-center p-4 shadow-sm shadow-neutral-300 mb-4 rounded-lg min-h-[90px] overflow-x-auto">
+    <section className="w-full flex justify-center items-center p-4 shadow-sm shadow-neutral-300 mb-4 rounded-lg min-h-[90px] overflow-x-auto relative z-0">
       {isLoading ? (
         <PropagateLoader size={8} color="red" />
       ) : (
