4c4da2e202dd2a0037f39bf6a1639ffd16a938f1
vpvm96 <vpvm96@gmail.com>
2023-05-11 16:22:18 +0900

    feat: 해당 카테고리에 따른 포스트 데이터 얻는 sanity 쿼리문 작성

--- service/posts.ts
+++ service/posts.ts
@@ -1,33 +1,29 @@
-import { SimplePost } from "@/model/post"
 import { client, urlFor } from "./sanity"
+import { SimplePost } from "@/model/post"
+
+const simplePostProjection = `
+...,
+"username": author->username,
+"userImage": author->image,
+"image": photo,
+"likes": likes[]->username,
+"text": comments[0].comment,
+"comments": count(comments),
+"id": _id,
+"createdAt": _createdAt,
+`
 
 export async function getFollowingPostOf(username: string) {
-  const simplePostProjection = `
-    ...,
-    "username": author->username,
-    "userImage": author->image,
-    "image": photo,
-    "likes": likes[]->username,
-    "text": comments[0].comment,
-    "comments": count(comments),
-    "id": _id,
-    "createdAt": _createdAt,
-  `
-
   return client
     .fetch(
       `
     *[_type == "post" && author->username == "${username}"
       || author._ref in *[_type == "user" && username == "${username}"].following[]._ref]
-      | order(_createdAt desc){${simplePostProjection}}
-  `
+      | order(_createdAt desc){
+        ${simplePostProjection}
+      }`
     )
-    .then((posts) =>
-      posts.map((post: SimplePost) => ({
-        ...post,
-        image: urlFor(post.image),
-      }))
-    )
+    .then(mapPosts)
 }
 
 export async function getPost(id: string) {
@@ -46,4 +42,44 @@ export async function getPost(id: string) {
     }`
     )
     .then((post) => ({ ...post, image: urlFor(post.image) }))
+}
+
+export async function getPostsOf(username: string) {
+  return client
+    .fetch(
+      `*[_type == "post" && author->username == "${username}"]
+      | order(_createdAt desc){
+        ${simplePostProjection}
+      }`
+    )
+    .then(mapPosts)
+}
+
+export async function getLikedPostsOf(username: string) {
+  return client
+    .fetch(
+      `*[_type == "post" && "${username}" in likes[]->username]
+      | order(_createdAt desc){
+        ${simplePostProjection}
+      }`
+    )
+    .then(mapPosts)
+}
+
+export async function getSavedPostsOf(username: string) {
+  return client
+    .fetch(
+      `*[_type == "post" && _id in *[_type == "user" && username=="${username}"].bookmarks[]._ref]
+      | order(_createdAt desc){
+        ${simplePostProjection}
+      }`
+    )
+    .then(mapPosts)
+}
+
+function mapPosts(posts: SimplePost[]) {
+  return posts.map((post: SimplePost) => ({
+    ...post,
+    image: urlFor(post.image),
+  }))
 }
