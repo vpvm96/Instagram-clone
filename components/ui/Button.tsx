9133d11e34713bf23ab73eb42ad67565e3a30d87
vpvm96 <vpvm96@gmail.com>
2023-05-12 19:38:40 +0900

    fix: Disable 처리를 위한 Props 추가

--- components/ui/Button.tsx
+++ components/ui/Button.tsx
@@ -2,14 +2,22 @@ interface Props {
   text: string
   onClick: () => void
   red?: boolean
+  disabled?: boolean
 }
 
-export default function Button({ text, onClick, red }: Props) {
+export default function Button({
+  text,
+  onClick,
+  red,
+  disabled = false,
+}: Props) {
   return (
     <button
       className={`border-none rounded-md py-2 px-8 text-white font-bold leading-4 ${
         red ? "bg-red-500" : "bg-sky-500"
-      }`}
+      } ${disabled && "opacity-80"}`}
+      disabled={disabled}
+      onClick={() => onClick()}
     >
       {text}
     </button>
