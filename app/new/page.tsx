215d383c9a5d00d6991133b2f2fad7f78adba57d
vpvm96 <vpvm96@gmail.com>
2023-05-12 19:41:34 +0900

    feat: Post 최상위 페이지 컴포넌트 Metadata 추가 및 완성

--- app/new/page.tsx
+++ app/new/page.tsx
@@ -1,3 +1,17 @@
-export default function NewPostPage() {
-  return <div>New Post Page</div>
+import { Metadata } from "next"
+import { redirect } from "next/navigation"
+import { getServerSession } from "next-auth"
+import { authOptions } from "@/pages/api/auth/[...nextauth]"
+import NewPost from "@/components/NewPost"
+
+export const metadata: Metadata = {
+  title: "New Post",
+  description: "Create a new post",
+}
+
+export default async function NewPostPage() {
+  const session = await getServerSession(authOptions)
+  if (!session?.user) redirect("/auth/signin")
+
+  return <NewPost user={session.user} />
 }
