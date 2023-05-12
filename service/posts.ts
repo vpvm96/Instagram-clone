a26e8200c35f06d2693dcf6f6201c9917bf381a7
vpvm96 <vpvm96@gmail.com>
2023-05-12 19:31:08 +0900

    feat: Create post GROQ query 작성

--- service/posts.ts
+++ service/posts.ts
@@ -1,3 +1,4 @@
+import { assetsURL } from "./sanity"
 import { client, urlFor } from "./sanity"
 import { SimplePost } from "@/model/post"
 
@@ -120,4 +121,33 @@ export async function addComment(
       },
     ])
     .commit({ autoGenerateArrayKeys: true })
+}
+
+export async function createPost(userId: string, text: string, file: Blob) {
+  return fetch(assetsURL, {
+    method: "POST",
+    headers: {
+      "content-type": file.type,
+      authorization: `Bearer ${process.env.SANITY_SECRET_TOKEN}`,
+    },
+    body: file,
+  })
+    .then((res) => res.json())
+    .then((result) => {
+      return client.create(
+        {
+          _type: "post",
+          author: { _ref: userId },
+          photo: { asset: { _ref: result.document._id } },
+          comments: [
+            {
+              comment: text,
+              author: { _ref: userId, _type: "reference" },
+            },
+          ],
+          likes: [],
+        },
+        { autoGenerateArrayKeys: true }
+      )
+    })
 }
