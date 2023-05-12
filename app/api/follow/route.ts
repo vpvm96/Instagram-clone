7d580f3a4252c8b5ededc05a327f1acc38647266
vpvm96 <vpvm96@gmail.com>
2023-05-12 19:32:29 +0900

    feat: 팔로우 기능 api요청 로직 작성

---
+++ app/api/follow/route.ts
@@ -0,0 +1,17 @@
+import { NextRequest, NextResponse } from "next/server"
+import { follow, unFollow } from "@/service/user"
+import { withSessionUser } from "@/utils/session"
+
+export async function PUT(req: NextRequest) {
+  return withSessionUser(async (user) => {
+    const { id: targetId, follow: isFollow } = await req.json()
+
+    if (!targetId || isFollow == null)
+      return new Response("Bad Request", { status: 400 })
+
+    const request = isFollow ? follow : unFollow
+    return request(user.id, targetId)
+      .then((res) => NextResponse.json(res))
+      .catch((error) => new Response(JSON.stringify(error), { status: 500 }))
+  })
+}
