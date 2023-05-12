9028fbfbacce833917f41e534f7cdb04cc5ecc33
vpvm96 <vpvm96@gmail.com>
2023-05-12 19:39:08 +0900

    refactor: Custom hooks로 코드 리팩토링

--- components/PostGrid.tsx
+++ components/PostGrid.tsx
@@ -1,19 +1,9 @@
-import useSWR from "swr"
-import { SimplePost } from "@/model/post"
+import { usePosts } from "@/hooks"
 import GridSpinner from "./ui/GridSpinner"
 import PostGridCard from "./PostGridCard"
 
-interface Props {
-  username: string
-  query: string
-}
-
-export default function PostGrid({ username, query }: Props) {
-  const {
-    data: posts,
-    isLoading,
-    error,
-  } = useSWR<SimplePost[]>(`/api/users/${username}/${query}`)
+export default function PostGrid() {
+  const { posts, isLoading } = usePosts()
 
   return (
     <div className="w-full text-center">
